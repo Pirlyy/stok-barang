5eb115526611b6a1905e7361a120dfcdc22effbb
Pirlyy <wfirlyari@gmail.com>
2025-10-07 11:06:18 +0700

    database baru

--- stokbarang.sql
+++ stokbarang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Sep 2025 pada 06.40
+-- Waktu pembuatan: 07 Okt 2025 pada 06.05
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -20,6 +20,57 @@ SET time_zone = "+00:00";
 --
 -- Database: `stokbarang`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang_keluars`
+--
+
+CREATE TABLE `barang_keluars` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `product_id` bigint(20) UNSIGNED NOT NULL,
+  `nama_penerima` varchar(255) DEFAULT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `barang_keluars`
+--
+
+INSERT INTO `barang_keluars` (`id`, `product_id`, `nama_penerima`, `nama_barang`, `jumlah`, `tanggal`, `created_at`, `updated_at`) VALUES
+(4, 24, 'mmk', 'Jawa Premium', 100, '2025-10-07 03:38:57', '2025-10-06 20:38:57', '2025-10-06 20:38:57'),
+(5, 31, 'me', 'oke ga', 121, '2025-10-07 03:50:29', '2025-10-06 20:50:29', '2025-10-06 20:50:29'),
+(6, 33, 'dilon', 'ikan', 10, '2025-10-07 03:55:00', '2025-10-06 20:55:00', '2025-10-06 20:55:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang_masuks`
+--
+
+CREATE TABLE `barang_masuks` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `product_id` bigint(20) UNSIGNED NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `supplier` varchar(255) DEFAULT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `barang_masuks`
+--
+
+INSERT INTO `barang_masuks` (`id`, `product_id`, `nama_barang`, `jumlah`, `supplier`, `tanggal`, `created_at`, `updated_at`) VALUES
+(1, 23, 're', 111, 'one team', '2025-10-03 08:34:47', '2025-10-03 01:34:47', '2025-10-03 01:34:47'),
+(2, 24, 'Jawa Premium', 120, 'Firr', '2025-10-03 08:36:46', '2025-10-03 01:36:46', '2025-10-03 01:36:46');
 
 -- --------------------------------------------------------
 
@@ -43,6 +94,23 @@ CREATE TABLE `cache_locks` (
   `key` varchar(255) NOT NULL,
   `owner` varchar(255) NOT NULL,
   `expiration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_barangs`
+--
+
+CREATE TABLE `data_barangs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `jumlah` int(11) NOT NULL DEFAULT 0,
+  `stok` int(11) NOT NULL DEFAULT 0,
+  `satuan` varchar(255) NOT NULL,
+  `supplier` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -115,7 +183,18 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '0001_01_01_000000_create_users_table', 1),
 (2, '0001_01_01_000001_create_cache_table', 1),
-(3, '0001_01_01_000002_create_jobs_table', 1);
+(3, '0001_01_01_000002_create_jobs_table', 1),
+(4, '2025_09_23_061758_create_personal_access_tokens_table', 2),
+(5, '2025_09_23_062320_create_products_table', 3),
+(6, '2025_09_23_064856_add_role_to_users_table', 4),
+(7, '2025_10_01_043939_add_supplier_to_products_table', 5),
+(8, '2025_10_01_053454_add_jumlah_penerima_to_products_table', 6),
+(9, '2025_10_01_073946_create_data_barangs_table', 7),
+(10, '2025_10_01_074047_create_barang_masuks_table', 7),
+(11, '2025_10_01_074212_create_barang_keluars_table', 7),
+(12, '2025_10_03_071556_create_barang_keluar_table', 8),
+(13, '2025_10_03_072300_add_product_id_to_barang_keluars_table', 8),
+(14, '2025_10_07_024040_add_image_to_products_table', 9);
 
 -- --------------------------------------------------------
 
@@ -132,6 +211,57 @@ CREATE TABLE `password_reset_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` text NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `supplier` varchar(255) DEFAULT NULL,
+  `jumlah` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `description` text DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `supplier`, `jumlah`, `price`, `description`, `image`, `created_at`, `updated_at`) VALUES
+(19, 'cihuyyy', 'cihuyy', 100, 12000.00, 'ini cimhuy', NULL, '2025-10-03 01:24:18', '2025-10-03 01:24:18'),
+(23, 're', 'one team', 111, 1000.00, 'aku', NULL, '2025-10-03 01:34:47', '2025-10-03 01:34:47'),
+(24, 'Jawa Premium', 'Firr', 20, 40000.00, 'Jawa yang suka sound horeg', '1759808316_mewing.jpg', '2025-10-03 01:36:46', '2025-10-06 20:38:57'),
+(25, 'indomie', 'yuu', 20, 5000.00, 'bawok', '1759807679_ya.jpg', '2025-10-06 20:27:59', '2025-10-06 20:27:59'),
+(30, 'Firr', 'dilon', 12, 3000.00, 'aku', '1759808219_ya.jpg', '2025-10-06 20:36:59', '2025-10-06 20:36:59'),
+(31, 'oke ga', 'ikanlele', 0, 10000.00, 'aku niga', '1759808992_mole.jpg', '2025-10-06 20:37:32', '2025-10-06 20:50:29'),
+(32, 'skibidi', 'si sigma asli jawa', 20000, 5000.00, 'manuk', '1759809196_hok.jpg', '2025-10-06 20:53:16', '2025-10-06 20:53:16'),
+(33, 'ikan enak', 'firly', 100, 200000.00, 'ikan iwak sedap', '1759809326_geshin impact.png', '2025-10-06 20:54:31', '2025-10-06 20:55:26');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `sessions`
 --
 
@@ -143,6 +273,13 @@ CREATE TABLE `sessions` (
   `payload` longtext NOT NULL,
   `last_activity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('I4S6wowas6FKskg4kPlMVxVEKFiBqYf4v0xe5XoR', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiRXNoek9XajhUQWRPV0Q4NW1aMm9ZdTZUQ2t3MG5FeDRUUE5TeGZ0TCI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czoyMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwIjt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1759809583);
 
 -- --------------------------------------------------------
 
@@ -158,12 +295,37 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `role` varchar(255) NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `role`) VALUES
+(1, 'stok', 'stokadmin@example.com', NULL, '$2y$12$P/LbXxOT/b1PfF1XsYj7AeFGsoI4J3d/kjuiausLLu080CgyGgcsi', NULL, '2025-09-22 23:55:17', '2025-09-22 23:55:17', 'admin'),
+(2, 'firr', 'firr@example.com', NULL, '$2y$12$DHgzrfh/ooyDPNthGIaeoeyk9UC5KcuuV/8dGJgeXul0YjaxY7PAu', NULL, '2025-09-23 00:26:30', '2025-09-23 00:26:30', 'admin'),
+(3, 'palpale', 'pale@example.com', NULL, '$2y$12$UQcQ9beHl/rh81rx3JDGOejtEnkU1CKoZqjooIFMBMkWQPyf/cpM2', NULL, '2025-09-23 00:29:29', '2025-09-23 00:29:29', 'user'),
+(4, 'mewing', 'ikan@email.com', NULL, '$2y$12$/DNaLmMZhYKs14se17LyluLZ0NrFsv8V.qRI7FLz6NvhJ.kEjOrJ.', NULL, '2025-10-06 20:51:26', '2025-10-06 20:51:26', 'user');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `barang_keluars`
+--
+ALTER TABLE `barang_keluars`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `barang_keluars_product_id_foreign` (`product_id`);
+
+--
+-- Indeks untuk tabel `barang_masuks`
+--
+ALTER TABLE `barang_masuks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indeks untuk tabel `cache`
@@ -176,6 +338,12 @@ ALTER TABLE `cache`
 --
 ALTER TABLE `cache_locks`
   ADD PRIMARY KEY (`key`);
+
+--
+-- Indeks untuk tabel `data_barangs`
+--
+ALTER TABLE `data_barangs`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `failed_jobs`
@@ -210,6 +378,21 @@ ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
 
 --
+-- Indeks untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`),
+  ADD KEY `personal_access_tokens_expires_at_index` (`expires_at`);
+
+--
+-- Indeks untuk tabel `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `sessions`
 --
 ALTER TABLE `sessions`
@@ -229,6 +412,24 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `barang_keluars`
+--
+ALTER TABLE `barang_keluars`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang_masuks`
+--
+ALTER TABLE `barang_masuks`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `data_barangs`
+--
+ALTER TABLE `data_barangs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT untuk tabel `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -244,13 +445,41 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `barang_keluars`
+--
+ALTER TABLE `barang_keluars`
+  ADD CONSTRAINT `barang_keluars_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `barang_masuks`
+--
+ALTER TABLE `barang_masuks`
+  ADD CONSTRAINT `barang_masuks_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
